b85be3e8e19d25863f4968ce6790827ffdb1e3ce
Chienchi Lo <chien-chi@pn2034281.lanl.gov>
2024-08-26 13:51:20 -0600

    update input.json example and shell script to submit to perlmutter

--- submit.sh
+++ submit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --qos=regular
 #SBATCH --time=12:00:00
-#SBATCH --output=/global/cfs/projectdirs/m3408/aim2/metagenome/MAGs/logs/test.log
+#SBATCH --output=/global/cfs/cdirs/m3408/aim2/metagenome/MAGs/logs/test.log
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task 32
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=your@email.com
-#SBATCH --constraint=haswell
+#SBATCH --constraint=cpu
 #SBATCH --account=m3408
 #SBATCH --job-name=MAGs_test
 
-cd /global/cfs/projectdirs/m3408/aim2/metagenome/MAGs
+cd /global/cfs/cdirs/m3408/aim2/metagenome/MAGs
 
-java -Dconfig.file=shifter.conf -jar /global/common/software/m3408/cromwell-45.jar run -i input.json  -m /global/cfs/projectdirs/m3408/aim2/metagenome/MAGs/metadataOut/test.json mbin_nmdc.wdl 
+java -Dconfig.file=shifter.conf -jar /global/common/software/m3408/cromwell-86.jar run -i input.json  -m /global/cfs/projectdirs/m3408/aim2/metagenome/MAGs/metadataOut/test.json mbin_nmdc.wdl 
